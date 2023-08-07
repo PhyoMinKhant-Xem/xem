b7aabe8f1358fcaa704bfdf0779a37d66b5fac74
PhyoMinKhantXem <phyominkhant.xem@gmail.com>
2023-08-07 11:20:13 +0630

    done

--- script.js
+++ script.js
@@ -29,14 +29,14 @@ let navLinks = document.querySelectorAll('header ul li a');
 window.onscroll = () =>{
 	sections.forEach(sec => {
 		let top = window.scrollY;
-		let offset = sec.offsetTop - 150;
+		let offset = sec.offsetTop - 200;
 		let height = sec.offsetHeight;
 		let id = sec.getAttribute('id');
 
 		if(top >= offset && top < offset + height){
 			navLinks.forEach(links => {
 				links.classList.remove('active');
-				document.querySelector('header ul li a[href*=' `${id}]`).classList.add('active');
+				document.querySelector('header ul li a[href*=' + id +']').classList.add('active');
 			});
 		};
 	});
